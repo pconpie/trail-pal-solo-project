d02b7fb96434c8825afe6d69ca154d6ec47de3a5
Patrick Connelly <patrickconnelly95@gmail.com>
2018-02-12 18:26:56 -0600

    add user controller

---
+++ server/public/scripts/controllers/user.controller.js
@@ -0,0 +1,6 @@
+app.controller('UserController', ['UserService', function(UserService) {
+  console.log('UserController created');
+  var self = this;
+  self.userService = UserService;
+  self.userObject = UserService.userObject;
+}]);
