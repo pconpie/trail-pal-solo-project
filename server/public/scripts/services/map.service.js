8f7434394f350264a5e7d0edd4ef208129c04713
Patrick Connelly <patrickconnelly95@gmail.com>
2018-02-12 18:27:36 -0600

    add map service

---
+++ server/public/scripts/services/map.service.js
@@ -0,0 +1,16 @@
+app.service('MapService', ['$http', function ($http) {
+    const self = this;
+    self.trailInfo = {};
+
+    self.getTrailInfo = function(lat, lon, id){
+        return $http.get(`/geoInfo/single/${lat}/${lon}/${id}`)
+           .then((response) => {
+                console.log('get geoInfo response ', response);
+                return response.data;
+                })
+            .catch((err) => {
+                console.log('get geoInfo err ', err);
+            })
+     }
+
+}]);
